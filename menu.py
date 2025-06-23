f160bbfadeb138eb1a7003532df4d8775a79132e
daang04 <117044840+daang04@users.noreply.github.com>
2025-06-22 22:52:26 -0500

    asdasds

---
+++ menu.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+# Título principal
+st.title("Plataforma de trazabilidad de insumos médicos")
+
+# Puedes agregar más elementos aquí según tus necesidades
+# Ejemplo: descripción, botones, formularios, etc.
